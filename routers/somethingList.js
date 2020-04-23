c81c18928cf5672d2ad0cc9081ad8a56688cf72b
iron <641016499@qq.com>
2020-04-23 13:18:01 +0800

    合并分支

--- routers/somethingList.js
+++ routers/somethingList.js
@@ -3,10 +3,16 @@ const Router = require("koa-router");
 const router = new Router();
 
 router.get("/list", async (ctx, next) => {
+  console.log(ctx.request.query.id, 999);
 
-  console.log(ctx.request.query.id,999)
   //创建mysql连接池
-  const sql = `select * from fruits where id = '${ctx.request.query.id}'`;
+  let sql = "select * from fruits";
+
+  if (ctx.request.query.id) {
+    let id = ctx.request.query.id;
+    sql = `select * from fruits where id = '${id}'`;
+  }
+
   const values = {};
   let { query } = require("../utils/dbObj");
   let res = await query(sql, values)
